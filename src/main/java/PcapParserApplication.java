5976f83aab8e3b5dec56052319e39e8e3ef30e46
gabriel <grossiwm@gmail.com>
2021-11-02 01:32:09 -0300

    Código formatado e otimização de importações

--- src/main/java/PcapParserApplication.java
+++ src/main/java/PcapParserApplication.java
@@ -5,18 +5,14 @@ import org.pcap4j.core.Pcaps;
 import org.pcap4j.packet.Packet;
 import org.pcap4j.packet.TcpPacket;
 
-import java.io.EOFException;
-import java.util.Objects;
-import java.util.concurrent.TimeoutException;
-
 public class PcapParserApplication {
 
-    public static void main(String[] args) throws PcapNativeException, NotOpenException, EOFException, TimeoutException {
+    public static void main(String[] args) throws PcapNativeException, NotOpenException {
 
         PcapHandle handle = Pcaps.openOffline("dump.pcap");
         Packet packet;
-        int httpCounter=0;
-        int httpsCounter=0;
+        int httpCounter = 0;
+        int httpsCounter = 0;
         while ((packet = handle.getNextPacket()) != null) {
 
             try {
@@ -37,8 +33,8 @@ public class PcapParserApplication {
 
         }
 
-        double securePercent = ((double) httpsCounter)/(httpsCounter+httpCounter)*100;
-        double unsecurePercent = ((double) httpCounter)/(httpsCounter+httpCounter)*100;
+        double securePercent = ((double) httpsCounter) / (httpsCounter + httpCounter) * 100;
+        double unsecurePercent = ((double) httpCounter) / (httpsCounter + httpCounter) * 100;
 
         System.out.println("safe: " + securePercent + " unsafe: " + unsecurePercent);
     }
