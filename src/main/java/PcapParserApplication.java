056f5941af2f8cc6bc7e5da5d7bd2a196c60d2a4
gabriel <grossiwm@gmail.com>
2021-11-21 18:22:23 -0300

    passando arquivo pcap por entrada do usuário

--- src/main/java/PcapParserApplication.java
+++ src/main/java/PcapParserApplication.java
@@ -1,22 +1,30 @@
+import org.pcap4j.core.PcapNativeException;
 import result.SafetyResult;
+import result.TcpConnectionsResult;
 import result.UdpConnectionsResult;
 import utils.PcapParserUtils;
+
+import java.util.Scanner;
 
 public class PcapParserApplication {
 
     public static void main(String[] args) {
-        PcapParserUtils pcapParserUtils = PcapParserUtils.forPcap("dump.pcap");
+        System.out.print("Type the dump file path: ");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        PcapParserUtils pcapParserUtils = PcapParserUtils.forPcap(input);
 
         try {
             SafetyResult safetyResult = pcapParserUtils.getSafetyResult();
             System.out.println(safetyResult);
 
-//            TcpConnectionsResult tcpPortsResult = pcapParserUtils.getTcpConnections();
-//            System.out.println(tcpPortsResult.getPortsPercentage());
+            TcpConnectionsResult tcpPortsResult = pcapParserUtils.getTcpConnections();
+            System.out.println(tcpPortsResult.getPortsPercentage());
 
             UdpConnectionsResult udpConnectionsResult = pcapParserUtils.getUdpConnections();
-            System.out.println(udpConnectionsResult.getPortsCountHashMap());
-            System.out.println(udpConnectionsResult.getTotalCount());
+            System.out.println(udpConnectionsResult.getPortsPercentage());
+        } catch (PcapNativeException pne) {
+            System.out.println("Couldn´t open pcap file of path " + input);
         } catch (Exception e) {
             e.printStackTrace();
         }
